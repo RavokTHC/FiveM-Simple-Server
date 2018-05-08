b57f02719dbe3608836f81dc35885172195136af
RavokTHC <samuelsahali42@gmail.com>
2018-05-08 14:54:35 -0300

    commit inicial

---
+++ run.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+echo _________________________________________________
+echo ####### #     #  #####  
+echo    #    #     # #     #       FIVE M SERVER
+echo    #    #     # #       
+echo    #    ####### #       
+echo    #    #     # #       
+echo    #    #     # #     #      TODA HORA CREW
+echo    #    #     #  #####     github.com/RavokTHC
+echo _________________________________________________
+
+
+pause
+start ..\run.cmd +exec server.cfg
+exit
